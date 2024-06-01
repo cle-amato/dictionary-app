7fd40557fac64534dee201048348fffe837145ea
cle-amato <cle.amato@gmail.com>
2024-06-01 17:50:54 +0100

    integrated photo API and component

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,38 +2,62 @@ import React, { useState } from "react";
 import "./Dictionary.css";
 import axios from "axios";
 import Result from "./Result";
+import Photos from "./Photos";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
   let [result, setResult] = useState(null);
+  let [photos, setPhotos] = useState(null);
+  let [loaded, setLoaded] = useState("");
 
-  function handleResponse(response) {
-    console.log(response.data);
+  function handleDictionaryResponse(response) {
     setResult(response.data);
+    setLoaded(true);
+  }
+
+  function handlePhotoResponse(response) {
+    setPhotos(response.data.photos);
   }
 
   function handleKeywordChange(event) {
     setKeyword(event.target.value);
-    console.log(keyword);
   }
 
   function search(event) {
     event.preventDefault();
     let apiKey = "ca0094f431ob9cbfef2ed6ce95bt0cc7";
     let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
-    axios.get(apiUrl).then(handleResponse);
+    let photoApiUrl = `https://api.shecodes.io/images/v1/search?query=${keyword}&key=${apiKey}`;
+
+    axios.get(apiUrl).then(handleDictionaryResponse);
+    axios.get(photoApiUrl).then(handlePhotoResponse);
   }
 
-  return (
-    <div className="Dictionary">
-      <section>
-        <form onSubmit={search}>
-          <p className="form-title">What word are you looking for?</p>
-          <input type="search" onChange={handleKeywordChange} />
-        </form>
-        <p className="form-hint">try searching for house, food, travel...</p>
-      </section>
-      <Result result={result} />
-    </div>
-  );
+  if (loaded) {
+    return (
+      <div className="Dictionary">
+        <section>
+          <form onSubmit={search}>
+            <p className="form-title">What word are you looking for?</p>
+            <input type="search" onChange={handleKeywordChange} />
+          </form>
+          <p className="form-hint">try searching for house, food, travel...</p>
+        </section>
+        <Result result={result} />
+        <Photos photos={photos} />
+      </div>
+    );
+  } else {
+    return (
+      <div className="Dictionary">
+        <section>
+          <form onSubmit={search}>
+            <p className="form-title">What word are you looking for?</p>
+            <input type="search" onChange={handleKeywordChange} />
+          </form>
+          <p className="form-hint">try searching for house, food, travel...</p>
+        </section>
+      </div>
+    );
+  }
 }
---
+++ src/Photos.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./Photos.css";
+
+export default function Photos(props) {
+  if (props.photos) {
+    return (
+      <section className="Photos">
+        <div className="row">
+          {props.photos.map(function (photo, index) {
+            return (
+              <div className=" col-6 col-md-4" key={index}>
+                <img
+                  src={photo.src.landscape}
+                  alt={photo.alt}
+                  className="img-fluid"
+                />
+              </div>
+            );
+          })}
+        </div>
+      </section>
+    );
+  } else {
+    return null;
+  }
+}
