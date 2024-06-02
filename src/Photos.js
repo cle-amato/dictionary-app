ab304bff14fc7f8207330fc41eea757e76a33d9f
cle-amato <cle.amato@gmail.com>
2024-06-02 20:59:30 +0100

    Fixed bug when photo array doesn't exist

--- src/Photos.js
+++ src/Photos.js
@@ -2,26 +2,30 @@ import React from "react";
 import "./Photos.css";
 
 export default function Photos(props) {
-  if (props.photos[0]) {
-    return (
-      <section className="Photos">
-        <div className="row">
-          {props.photos.map(function (photo, index) {
-            return (
-              <div className=" col-6 col-md-4" key={index}>
-                <a href={photo.url} target="_blank" rel="noreferrer">
-                  <img
-                    src={photo.src.landscape}
-                    alt={photo.alt}
-                    className="img-fluid"
-                  />
-                </a>
-              </div>
-            );
-          })}
-        </div>
-      </section>
-    );
+  if (props.photos) {
+    if (props.photos[0]) {
+      return (
+        <section className="Photos">
+          <div className="row">
+            {props.photos.map(function (photo, index) {
+              return (
+                <div className=" col-6 col-md-4" key={index}>
+                  <a href={photo.url} target="_blank" rel="noreferrer">
+                    <img
+                      src={photo.src.landscape}
+                      alt={photo.alt}
+                      className="img-fluid"
+                    />
+                  </a>
+                </div>
+              );
+            })}
+          </div>
+        </section>
+      );
+    } else {
+      return null;
+    }
   } else {
     return null;
   }
